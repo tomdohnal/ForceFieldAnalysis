1e967a37445d3a0dae8c5a44071a70c9ac1a1272
Tom Dohnal <tomas.dohnal@gmail.com>
2017-11-12 20:14:30 +0100

    Change Container finished. Can enter and edit change name and description.

--- src/containers/ChangeContainer.js
+++ src/containers/ChangeContainer.js
@@ -1,7 +1,8 @@
 // @flow
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Input, Button } from 'semantic-ui-react';
+import { Form, Input, TextArea, Button, Icon } from 'semantic-ui-react';
+import EditIcon from 'react-icons/lib/ti/edit';
 
 import { COLORS } from '../constants/styles';
 import type { ReduxState } from '../redux/index';
@@ -25,35 +26,88 @@ class ChangeContainer extends Component<Props, State> {
     editChangeDescription: !this.props.change.description,
   };
 
-   onButtonClick = editChangeBoolean => (): void => {
-     this.setState({ [editChangeBoolean]: false });
-   };
+  onNameFormSubmit = (): void => {
+    this.setState({ editChangeName: false });
+  };
 
-   renderForm(onChange: (value: string) => void, onButtonClick: (value: string) => void) {
-     return (
-       <Box>
-         <Input onChange={onChange} />
-         <Button onClick={onButtonClick} />
-       </Box>
-     );
-   }
+  onEditNameClick = (): void => {
+    this.setState({ editChangeName: true });
+  };
 
-   render() {
-     const { change: { name, description }, onNameChange, onDescriptionChange } = this.props;
+  onDescriptionFormSubmit = (): void => {
+    this.setState({ editChangeDescription: false });
+  };
 
-     return (
-       <Box padding="20px 16px" color={COLORS.BLUE} height="100%">
-         {this.state.editChangeName ?
-           this.renderForm(onNameChange, this.onButtonClick('editChangeName')) :
-           <Text>{name}</Text>
-         }
-         {this.state.editChangeDescription ?
-           this.renderForm(onDescriptionChange, this.onButtonClick('editChangeDescription')) :
-           <Text>{description}</Text>
-         }
-       </Box>
-     );
-   }
+  onEditDescriptionClick = (): void => {
+    this.setState({ editChangeDescription: true });
+  };
+
+  render() {
+    const { change: { name, description }, onNameChange, onDescriptionChange } = this.props;
+
+    return (
+      <Box padding="20px 16px" color={COLORS.BLUE} height="100%">
+        {this.state.editChangeName ?
+          <Form onSubmit={this.onNameFormSubmit}>
+            <Form.Group>
+              <Input
+                size="massive"
+                style={{ width: '100%' }}
+                onChange={onNameChange}
+                placeholder="Enter the name of your change"
+                value={name || ''}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Button>Submit Name</Button>
+            </Form.Group>
+          </Form>
+          :
+          <Box marginTop="16px" textAlign="center">
+            <Text color={COLORS.WHITE} fontSize="48px">{name}</Text>
+            <Box display="inline" >
+              <EditIcon
+                color={COLORS.WHITE}
+                size="28"
+                style={{ verticalAlign: 'top', marginLeft: '8px', cursor: 'pointer' }}
+                onClick={this.onEditNameClick}
+              />
+            </Box>
+          </Box>
+        }
+        {this.state.editChangeDescription ?
+          <Box marginTop="16px">
+            <Form onSubmit={this.onDescriptionFormSubmit}>
+              <Form.Group>
+                <TextArea
+                  autoHeight
+                  onChange={onDescriptionChange}
+                  placeholder="Enter the description of your change (optional)"
+                  value={description || ''}
+                />
+              </Form.Group>
+              <Form.Group>
+                <Button>Submit Description</Button>
+                <Button compact size="mini">No description</Button>
+              </Form.Group>
+            </Form>
+          </Box>
+          :
+          <Box marginTop="16px">
+            <Text color={COLORS.WHITE} fontSize="18px">{description}</Text>
+            <Box display="inline" >
+              <EditIcon
+                color={COLORS.WHITE}
+                size="18"
+                style={{ verticalAlign: 'top', marginLeft: '4px', cursor: 'pointer' }}
+                onClick={this.onEditDescriptionClick}
+              />
+            </Box>
+          </Box>
+        }
+      </Box>
+    );
+  }
 }
 
 const mapStateToProps = (state: ReduxState) => ({
