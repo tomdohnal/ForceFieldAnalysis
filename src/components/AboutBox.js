df1d4058a6fc53c05f67200c23ff679b99343bc3
Tom Dohnal <tomas.dohnal@gmail.com>
2018-01-14 16:21:19 +0100

    AboutBox text updated.

--- src/components/AboutBox.js
+++ src/components/AboutBox.js
@@ -32,7 +32,7 @@ const AboutBox = ({
     </Text>
     <Text fontSize="32px" marginTop="16px">What is Force Field Analysis?</Text>
     <Divider hidden />
-    <Text fontSize="18px">Force Field Analysis is a decision-making tool. Originally created in 1940s by psychologist Kurt Lewin, it is widely used in business for making decisions. The idea is to create driving (also called helping) forces that are driving change and hindering (also called resisting) forces that are hindering change. You can attribute different strengths to each force. Once you&#39;ve done with that, you can compare the sum of your driving forces and your hindering forces and decide if you should go or not go for change!</Text>
+    <Text fontSize="18px">Force Field Analysis is a decision-making tool. Originally created in 1940s by psychologist Kurt Lewin, it is widely used in business for making decisions. The idea behind it is to create driving forces that are driving your change and hindering forces that are hindering your change. You can attribute different strengths to each of the forces. Once you&#39;ve done with that, you can compare the sum of your driving forces and your hindering forces and decide if you should or shouldn&#39;t go for the change!</Text>
   </Box>
 );
 
